9d6058d9703755e754d84613b5d3df667f023606
Nikki Pantony <nikkipantony@gmail.com>
2021-02-10 21:04:18 +0000

    Remove Header, Simplify Footer

--- src/05_Organisms/Footer/Footer.js
+++ src/05_Organisms/Footer/Footer.js
@@ -7,33 +7,13 @@
  */
 
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import ExLink from "../../03_Atoms/ExLink/ExLink"
 
 const Footer = () => {
-    const data = useStaticQuery(graphql`
-        query FooterSiteTitleQuery {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `)
-
     return (
         <footer className="footer">
             <div className="footer__inner">
                 <p className="footer__text">
-                    <small>
-                        &copy; {new Date().getFullYear()}{" "}
-                        {data.site.siteMetadata.title}. Built by{" "}
-                        <ExLink
-                            to="https://nikkipantony.com"
-                            text="Nikki Pantony"
-                            modifier="--microcopy"
-                        />
-                    </small>
+                    <small>&copy; {new Date().getFullYear()}</small>
                 </p>
             </div>
         </footer>
