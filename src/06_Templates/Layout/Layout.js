c9fc777023fa5818085d68366003a35ec0aa488f
Nikki Pantony <nikkipantony@gmail.com>
2021-02-15 16:17:10 +0000

    Add dark mode styling to Avatar

--- src/06_Templates/Layout/Layout.js
+++ src/06_Templates/Layout/Layout.js
@@ -8,9 +8,9 @@ import Footer from "../../05_Organisms/Footer/Footer"
 
 const Layout = ({ children }) => (
     <>
-        {/* <div className=""> */}
-        {/* <div className="light-mode"> */}
-        <div className="dark-mode">
+        <div className="">
+            {/* <div className="light-mode"> */}
+            {/* <div className="dark-mode"> */}
             <div className="layout">
                 <main className="layout__main">{children}</main>
             </div>
