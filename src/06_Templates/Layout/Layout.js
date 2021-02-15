f216750c9f9579f717fb8b64231f01eed0b1a016
Nikki Pantony <nikkipantony@gmail.com>
2021-02-15 13:52:33 +0000

    Update social media icon dark mode colors

--- src/06_Templates/Layout/Layout.js
+++ src/06_Templates/Layout/Layout.js
@@ -8,12 +8,14 @@ import Footer from "../../05_Organisms/Footer/Footer"
 
 const Layout = ({ children }) => (
     <>
-        {/* <div className="layout"> */}
-        {/* <div className="layout light-mode"> */}
-        <div className="layout dark-mode">
-            <main className="layout__main">{children}</main>
+        {/* <div className=""> */}
+        {/* <div className="light-mode"> */}
+        <div className="dark-mode">
+            <div className="layout">
+                <main className="layout__main">{children}</main>
+            </div>
+            <Footer />
         </div>
-        <Footer />
     </>
 )
 
