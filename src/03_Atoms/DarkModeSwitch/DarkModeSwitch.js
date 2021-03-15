d6de73781f96f102256be1a6341e2be3f421f9e9
Nikki Pantony <nikkipantony@gmail.com>
2021-03-15 08:18:48 +0000

    Update Avatar image with cartoon version, Update toggle styles

--- src/03_Atoms/DarkModeSwitch/DarkModeSwitch.js
+++ src/03_Atoms/DarkModeSwitch/DarkModeSwitch.js
@@ -3,9 +3,12 @@ import React from "react"
 const DarkModeSwitch = () => {
     const toggle = () => document.body.classList.toggle("dark-mode")
 
-    // TODO: Update so it is Ally complient!
     return (
         <form className="dark-mode-switch">
+            <label htmlFor="dark-mode" className="dark-mode-switch__label">
+                Dark Mode
+            </label>
+
             <input
                 type="checkbox"
                 id="dark-mode"
@@ -17,11 +20,7 @@ const DarkModeSwitch = () => {
 
             <div className="dark-mode-switch__toggle"></div>
 
-            <label htmlFor="dark-mode" className="dark-mode-switch__label">
-                <span className="sr-only">
-                    Switch between light and dark mode
-                </span>
-            </label>
+            <div className="dark-mode-switch__toggle-line"></div>
         </form>
     )
 }
