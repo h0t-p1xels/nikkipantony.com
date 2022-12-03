14fe7a9821dd6eb3af3bf96cc8187e9df3f5ba45
Nikki Pantony <nikkipantony@gmail.com>
2022-12-03 19:36:30 +0000

    Update Page Template

--- src/5_Templates/PageTemplate/PageTemplate.js
+++ src/5_Templates/PageTemplate/PageTemplate.js
@@ -1,20 +1,22 @@
 /*
- * Main Layout Template Component
+ * Page Template Component
  *
  */
 
 import React from "react"
-// import DarkMode from "../../09_Utilities/DarkMode"
+import DarkMode from "../../8_Utilities/DarkMode"
+import Header from "../../4_Organisms/Header/Header"
 import Footer from "../../4_Organisms/Footer/Footer"
 
-const Layout = ({ children }) => (
+const PageTemplate = ({ children }) => (
   <>
-    {/* <DarkMode /> */}
+    <DarkMode />
     <div className="page-template">
+      <Header />
       <main className="page-template__main">{children}</main>
+      <Footer />
     </div>
-    <Footer />
   </>
 )
 
-export default Layout
+export default PageTemplate
