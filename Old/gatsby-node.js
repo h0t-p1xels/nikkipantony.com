820bdcfdc4c037217dbf809493b8f41f6ec98885
Nikki Pantony <nikkipantony@gmail.com>
2022-12-04 01:03:13 +0000

    Delete older components

--- Old/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-/*
- * Gatsby Node API
- *
- * Implement `Async/await`, `Promise` and `Callback` APIs from this file.
- *
- * Gatsby Node API: https://www.gatsbyjs.org/docs/node-apis
- */
--- Old/gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-/*
- * Gatsby's SSR APIs
- *
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * Gatsby's SSR APIs: https://www.gatsbyjs.org/docs/ssr-apis/
- */
